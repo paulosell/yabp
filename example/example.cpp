dd54bf902d5b760a7fa8465e67b53a7063481ebe
Paulo Sell <sellpaulof@gmail.com>
2024-01-28 16:37:00 -0300

    refactor: update example

--- example/example.cpp
+++ example/example.cpp
@@ -1,4 +1,4 @@
-#include "../include/base64url.h"
+#include "base64url.h"
 #include <iostream>
 #include <string.h>
 
@@ -7,16 +7,17 @@ int main() {
   char decoded[16];
   char hello_world_decoded[] = "Hello, World!";
   char hello_world_encoded[] = "SGVsbG8sIFdvcmxkIQ";
+  Base64Url::Base64Url b;
 
-  base64url::encode(reinterpret_cast<uint8_t *>(hello_world_decoded),
+  b.encode(reinterpret_cast<uint8_t *>(hello_world_decoded),
                     strlen(hello_world_decoded),
                     reinterpret_cast<uint8_t *>(encoded), sizeof(encoded));
-  std::cout << "Hello, World! encoded to Base64URL: ";
+  std::cout << "Hello, World! encoded to Base64Url: ";
   std::cout << encoded << '\n';
 
-  base64url::decode(reinterpret_cast<uint8_t *>(hello_world_encoded),
+  b.decode(reinterpret_cast<uint8_t *>(hello_world_encoded),
                     strlen(hello_world_encoded),
                     reinterpret_cast<uint8_t *>(decoded), sizeof(decoded));
-  std::cout << "SGVsbG8sIFdvcmxkIQ decoded from Base64URL: ";
+  std::cout << "SGVsbG8sIFdvcmxkIQ decoded from Base64Url: ";
   std::cout << decoded << '\n';
 }
